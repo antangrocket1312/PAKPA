7948534bbfb70ba7d4516e702d1e71aeea91e23c
Tony Tang <antangrocket1312@gmail.com>
2024-02-26 21:06:44 +0700

    Minor update prompting code structure

--- script/prompted_absa.py
+++ script/prompted_absa.py
@@ -1,9 +1,5 @@
-import argparse
-import pandas as pd
-import json
-import openai
-import time
-import sys, os
+import os
+import sys
 sys.path.insert(1, os.path.join(sys.path[0], '../'))
 from utils.prompting import *
 from utils.preprocessing import preprocessing_comment_data
@@ -11,12 +7,90 @@ from multiprocessing import Pool
 import re
 import ast
 import spacy
+import argparse
+import pandas as pd
+import json
+import openai
+import time
 
 nlp = spacy.load('en_core_web_sm')
 openai.api_key = "EMPTY"  # Not support yet
 openai.api_base = "http://localhost:8000/v1"
-
 model = "vicuna-7b-v1.3"
+
+
+def get_absa_completion(comment, offset=1, model="vicuna-7b-v1.3"):
+    absa_prompt = get_prompt("absa_few_shot") + "\n\nReview sentence: %s\n"
+    prompt = absa_prompt % comment
+    comment_len = len(comment.split())
+    max_tokens = int(comment_len) * 5
+    max_tokens = max(50, max_tokens) * offset
+
+    return get_completion(model, prompt, max_tokens)
+
+
+def prompt_based_absa(root_path, domain, domain_df, save_step=10):
+    src_path = f"{root_path}/{domain}"
+    Path(src_path).mkdir(parents=True, exist_ok=True)
+    absa_extractions = []
+
+    file_names = listdir(src_path)
+    postfix = [re.split("[_.]", name)[1]
+               for name in listdir(src_path)
+               ]
+    start = 0
+    if 'done' in postfix:
+        print(domain, ": ", "Loaded saved ABSA file. Done")
+        new_domain_df = pd.read_pickle(f"{src_path}/{domain}_done.pkl")
+        return new_domain_df
+    elif len(postfix) > 0:
+        last_index = max([int(idx) for idx in postfix if idx != 'done'])
+        last_domain_df = pd.read_pickle(f"{src_path}/{domain}_{last_index}.pkl")
+        absa_extractions = last_domain_df['absa_extractions'].tolist()
+        start = last_index
+        print(domain, "Loaded saved ABSA file. Continuing")
+    else:
+        print(domain, "Start new process.")
+
+    for i, (_, row) in tqdm(enumerate(domain_df.iterrows()), total=domain_df.shape[0]):
+        if i < start:
+            continue
+
+        comment = row['sentences']
+        comment = re.sub('^\n+', '', comment)
+
+        offset = 1
+        answers = []
+        prev_response = ""
+        while len(answers) == 0:
+            try:
+                response = get_absa_completion(comment, offset=offset)
+            except:
+                answers = ['[]']
+                break
+            response = response.replace("\_", "_")
+
+            answers = re.findall('Answer: (\[.*\])', response)
+            offset += 1
+            #             print(offset)
+            if response == prev_response or offset >= 5:
+                answers = ['[]']
+                break
+            prev_response = response
+
+        absa_extraction = answers[0]
+        absa_extractions += [absa_extraction]
+
+        if (i + 1) % save_step == 0:
+            save_df = domain_df.iloc[:i + 1]
+            save_df.insert(0, 'absa_extractions', absa_extractions)
+            save_df.to_pickle(f"{src_path}/{domain}_{i + 1}.pkl")
+
+    new_domain_df = domain_df.iloc[:i + 1]
+    new_domain_df.insert(0, 'absa_extractions', absa_extractions)
+    new_domain_df.to_pickle(f"{src_path}/{domain}_done.pkl")
+    return new_domain_df
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
